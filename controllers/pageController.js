4adfedc3fbb4c298d76fd884080c977c1d155c39
ataberkdw <ataberkturkoglug3@gmail.com>
2021-11-04 05:09:56 +0300

    contact sayfasi ve mail islemleri olusturuldu

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodeMailer = require('nodemailer');
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render('index', {
@@ -25,4 +27,34 @@ exports.getLoginPage = (req,res)=>{
   res.status(200).render('login',{
     page_name:'login'
   })
+}
+exports.getContactPage = (req,res) =>{
+  res.status(200).render('contact',{
+    page_name:'contact'
+  })
+}
+exports.sendEmail = async (req,res) =>{
+    const outputMessage = `
+      <h1>Mail Details</h1>
+      <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Messages</li>
+        <p>${req.body.message}</p>
+      </ul>
+    `;
+    let transporter = nodeMailer.createTransport({
+      host: "smtp.gmail.com",
+      port:465,
+      secure:true,
+      auth:{
+        
+      }
+    });
+    let info = await transporter.sendMail({
+      from: '"Smart EDU Contact Form" <ataberkturkoglug3@gmail.com>',
+      to: "ataberkturkoglug3@gmail.com",
+      html:outputMessage
+    });
+    res.status(200).redirect('contact');
 }
